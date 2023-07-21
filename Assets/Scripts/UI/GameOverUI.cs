fec7bc57fcff700ce66cc1d972e0b719fa9b2746
Matt Moore <thewmoore@gmail.com>
2023-07-20 22:58:38 -0400

    Add game over text field references.

---
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOverUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI winnerNameText;
+    [SerializeField] private PlayerScoreTemplate playerScoreTemplate;
+
+    public Dictionary<ulong, int> _playerScores;
+    // TODO: dynamically create list of players + scores
+
+    private void Start()
+    {
+        Hide();
+    }
+
+    public void Show(string winnerName, Dictionary<ulong, int> playerScores)
+    {
+        winnerNameText.text = winnerName;
+        _playerScores = playerScores;
+        
+        gameObject.SetActive(true);
+    }
+    public void Hide() => gameObject.SetActive(false);
+}
---
+++ Assets/Scripts/UI/PlayerScoreTemplate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerScoreTemplate : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI PlayerNameText;
+    [SerializeField] public TextMeshProUGUI PlayerScoreText;
+}
